fd8a9624fae17d01fbb9bbf6ef38c0352a7f9c15
Hassan Agmir <hagmir6@gmail.com>
2023-10-29 17:44:25 +0000

    Best commit

---
+++ components/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <img class="logo" src="../images/logogy.png" alt="logo">
+    <ul>
+        <li><a href="/">Home</a> </li>
+        <li><a href="/pages/calories.php">Sources of calories</a> </li>
+        <li><a href="/pages/supplements.php">Nutritional supplements</a> </li>
+        <li><a href="#footer">Cantact</a> </li>
+    </ul>
+</nav>
