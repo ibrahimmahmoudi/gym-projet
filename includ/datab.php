329bfc77012ffbcac881704becabeb7bde463d5f
Hassan Agmir <hagmir6@gmail.com>
2023-10-29 19:11:22 +0000

    Beset commit

--- includ/datab.php
+++ includ/datab.php
@@ -1,13 +1,26 @@
-<?php 
+<?php
 
-$hst = 'mysql:host=localhost;port=3306;dbname=gym' ;
-$usuer = 'root' ;
-$pass = '' ;
-try{
-    $db = new PDO($hst ,$usuer  ,$pass);
-}
-catch(PDOException $e){
-    echo 'failed' . $e->getMessage();
+require_once('../vendor/autoload.php'); // Load Composer's autoloader
+
+// Load environment variables from .env
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+
+$dbName = $_ENV['DB_NAME'];
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
+
+
+
+try {
+    $db = new PDO(
+        "mysql:host=$dbHost;dbname=$dbName;charset=utf8",
+        $dbUser,
+        $dbPass
+    );
+} catch (PDOException $e) {
+    echo 'Failed: ' . $e->getMessage();
 }
 
 
